543683600062df1761f0b1a5391dae9ab5644cd6
Louis <louisedwell@stackit.cloud>
2025-05-05 13:33:53 +0200

    xUnit Test für Torus Spielfeld

--- test/GOL.Business.test/GameEngineTest.cs
+++ test/GOL.Business.test/GameEngineTest.cs
@@ -17,16 +17,33 @@ namespace GOL.Tests
         [Fact]
         public void CountNeighbors_ShouldReturnCorrectNumber()
         {
+            //arrange
             var grid = new List<Cell>
             {
                 new Cell { X = 1, Y = 1, IsAlive = true },
                 new Cell { X = 2, Y = 1, IsAlive = true },
                 new Cell { X = 1, Y = 2, IsAlive = true }
             };
-
+            //act
        
             int count = _engine.CountNeighbors(grid, grid[0], 5, 5);
+            //assert
+            Assert.Equal(2, count); // 2 lebendige Nachbarn
+        }
 
+        public void CountNeighbors_ShouldWrapAndReturnCorrectNumber()
+        {
+            //arrange
+            var grid = new List<Cell>
+            {
+                new Cell { X = 0, Y = 0, IsAlive = true },
+                new Cell { X = 4, Y = 4, IsAlive = true },
+                new Cell { X = 0, Y = 4, IsAlive = true }
+            };
+            //act
+
+            int count = _engine.CountNeighbors(grid, grid[0], 5, 5);
+            //assert
             Assert.Equal(2, count); // 2 lebendige Nachbarn
         }
 
@@ -85,15 +102,16 @@ namespace GOL.Tests
         [Fact]
         public void NextGeneration_ShouldUpdateGridCorrectly()
         {
+            // Arrange
             var grid = new List<Cell>
             {
                 new Cell { X = 1, Y = 0, IsAlive = true },
                 new Cell { X = 1, Y = 1, IsAlive = true },
                 new Cell { X = 1, Y = 2, IsAlive = true }
             };
-
+            //act
             var nextGen = _engine.NextGeneration(grid, 5, 5);
-
+            //assert
             Assert.Contains(nextGen, c => c.X == 0 && c.Y == 1); // sollte leben
             Assert.Contains(nextGen, c => c.X == 1 && c.Y == 1); // sollte leben
             Assert.Contains(nextGen, c => c.X == 2 && c.Y == 1); // sollte leben
@@ -102,15 +120,16 @@ namespace GOL.Tests
         [Fact]
         public void NextGeneration_ShouldUpdateAndWrapGridCorrectly()
         {
+            // Arrange
             var grid = new List<Cell>
                 {
                   new Cell { X = 9, Y = 9, IsAlive = true },
                   new Cell { X = 9, Y = 0, IsAlive = true },
                   new Cell { X = 9, Y = 1, IsAlive = true }
                 };
-
+            //act
             var nextGen = _engine.NextGeneration(grid, 10, 10);
-
+            //assert
             Assert.Contains(nextGen, c => c.X == 9 && c.Y == 0); // sollte leben
             Assert.Contains(nextGen, c => c.X == 8 && c.Y == 0); // sollte leben
             Assert.Contains(nextGen, c => c.X == 0 && c.Y == 0); // sollte leben
